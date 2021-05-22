0ad8807a4cc1b644f1736c275a682c3caa3a9772
Juan Pablo <juanpablopela.34cj@gmail.com>
2021-05-21 22:15:42 -0500

    v3

--- index.php
+++ index.php
@@ -18,11 +18,11 @@
         <div class="icon cancel-btn">
           <i class="fas fa-times"></i>
         </div>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Features</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Menú</a></li>
+        <li><a href="#">Bodega</a></li>
+        <li><a href="#">Iniciar Sesión</a></li>
+        <li><a href="#">Registrarse</a></li>
       </ul>
       <div class="icon menu-btn">
         <i class="fas fa-bars"></i>
@@ -34,10 +34,18 @@
   <div class="about">
     <div class="content">
       <div class="title">
-        BIENVENIDOS AL DELL'SON RISTORANTE 
+        BIENVENIDOS A DELL'SON RISTORANTE
       </div>
       <p>
-        Aquí encontrarás comida Mexicana, Italiana, Colombiana y muchas más culturas que te hará la boca agua!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui sapiente
+        recusandae reprehenderit tempora est! Voluptates quaerat iste eius
+        nisi minima repudiandae. Delectus doloremque ratione sint eum
+        mollitia, pariatur esse unde quibusdam atque commodi impedit voluptas,
+        ipsam nesciunt natus. Iure aliquam eos libero dignissimos animi,
+        debitis repudiandae expedita possimus nemo odio laudantium quo modi
+        quas fuga, non, eum voluptatum amet pariatur ducimus recusandae
+        laboriosam? Consectetur odio omnis odit placeat temporibus. Aut
+        debitis minima accusantium beatae distinctio, aliquam amet!
       </p>
     
     </div>
@@ -45,29 +53,29 @@
 
   <!-- ------------------------------------------------------------------------------------- -->
 
-  <h1 class="title">Card con efecto hover - Magtimus</h1>
+  <h1 class="title"> Nuestra especialidad</h1>
     
     <div class="container">
        
         <div class="card">
             <img src="../ImagenesRestaurante/pasta.jpg">
-            <h4>Naturaleza</h4>
+            <h4>Pasta Italiana</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
-            <a href="#">Leer más</a>
+            <a href="#">Ver más platos</a>
         </div>
         
         <div class="card">
             <img src="../ImagenesRestaurante/tacos.jpg">
-            <h4>Comida</h4>
+            <h4>Tacos</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
-            <a href="#">Leer más</a>
+            <a href="#">Ver más platos</a>
         </div>
         
         <div class="card">
             <img src="../ImagenesRestaurante/sandwich.jpg">
-            <h4>Tecnología</h4>
+            <h4>Sánduches</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
-            <a href="#">Leer más</a>
+            <a href="#">Ver más platos</a>
         </div>
         
     </div>
